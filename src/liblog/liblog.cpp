47f04db0b38b38fdd790b8dbe43a3b44b9fc98ff
Brendan Gowen <bgowengaming1@gmail.com>
2018-07-16 04:49:31 -0400

    Updated liblog to version in my GitHub repo & upcoming CCalc update.

--- src/liblog/liblog.cpp
+++ src/liblog/liblog.cpp
@@ -1,8 +1,8 @@
 #include "liblog.hpp"
 
 #include <iostream>
+#include <sstream>
 #include <string>
-
 
 logger::logger(loglevel_t loglevel, std::string logfile)
 {
@@ -72,25 +72,115 @@ bool logger::log(std::string log_str)
     return false;
 }
 
+bool logger::log_value(std::string log_str, int value)
+{
+    if (m_loglevel >= loglevel_t::everything)
+    {
+        /* Credit: https://stackoverflow.com/questions/332111/how-do-i-convert-a-double-into-a-string-in-c
+         *
+         * std::ostringstream strs;
+         * strs << dbl;
+         * std::string str = strs.str(); */
+
+        std::ostringstream casted_value;
+        casted_value << value;
+
+        std::string message = BLANK_STR;
+
+        if(log_str != "")
+        {
+            message = log_str + casted_value.str() + "\n";
+        }
+
+        else
+        {
+            message = casted_value.str() + "\n";
+        }
+
+        log_history.push_back(message);
+
+        return true;
+    }
+
+    return false;
+}
+
+bool logger::log_value(std::string log_str, double value)
+{
+    if (m_loglevel >= loglevel_t::everything)
+    {
+        /* Credit: https://stackoverflow.com/questions/332111/how-do-i-convert-a-double-into-a-string-in-c
+         *
+         * std::ostringstream strs;
+         * strs << dbl;
+         * std::string str = strs.str(); */
+
+        std::ostringstream casted_value;
+        casted_value << value;
+
+        std::string message = BLANK_STR;
+
+        if(log_str != "")
+        {
+            message = log_str + casted_value.str() + "\n";
+        }
+
+        else
+        {
+            message = casted_value.str() + "\n";
+        }
+
+        log_history.push_back(message);
+
+        return true;
+    }
+
+    return false;
+}
+
+bool logger::print(std::string print_str)
+{
+    if (m_loglevel >= loglevel_t::print)
+    {
+        std::string message = "[OUTPUT] " + print_str + "\n";
+
+        std::cout << message;
+        log_history.push_back(message);
+
+        return true;
+    }
+
+    return false;
+}
+
+bool logger::just_print(std::string print_str)
+{
+    if (m_loglevel >= loglevel_t::print)
+    {
+        std::cout << print_str;
+        log_history.push_back(print_str);
+
+        return true;
+    }
+
+    return false;
+}
+
 bool logger::dump_log(std::string file)
 {
-    if(!log_history.empty())
+    std::ofstream log_file;
+    log_file.open(file);
+
+    if(log_file.is_open())
     {
-        std::ofstream log_file;
-        log_file.open(file);
-
-        if(log_file.is_open())
+        for(auto x : log_history)
         {
-            for(auto x : log_history)
-            {
-                log_file << x;
-            }
-            
-            log_file.close();
-            log_history.clear();
-
-            return true;
+            log_file << x;
         }
+        
+        log_file.close();
+
+        return true;
     }
 
     return false;
--- src/liblog/liblog.hpp
+++ src/liblog/liblog.hpp
@@ -4,8 +4,11 @@
 #include <string>
 #include <vector>
 
+#define BLANK_STR ""
+
 enum class loglevel_t
 {
+    print = 0,
     errors = 1,
     warnings = 2,
     everything = 3
@@ -23,9 +26,18 @@ public:
     // when object is deconstructed, all log history is flushed from the vector into a log file
     ~logger();
 
+    // logging functions
     bool log_error(std::string error_str);
     bool log_warning(std::string warning_str);
     bool log(std::string log_str);
+
+    bool log_value(std::string log_str, int value);
+    bool log_value(std::string log_str, double value);
+
+    // print function
+    // goes into log_history to be flushed unlike std::cout
+    bool print(std::string print_str);
+    bool just_print(std::string print_str);
 
     // called by deconstructor, but can also be called manually
     bool dump_log(std::string file);
